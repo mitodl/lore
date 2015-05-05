fd2cdb38a7034cfe1f9f14deea7db6c278d337c0
Carson Gee <x@carsongee.com>
2015-05-05 18:41:09 -0400

    Create basic django project foundation - Load configuration from environment or yaml file - Use dj-static for static file serving - Add docker images for postgresql based development environment - Add instructions to  README for getting started.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# lore
-placeholder for LORE
+# LORE
+
+## Getting Started
+ You can either run this locally with a default sqlite database after
+ installing the requirements.txt file, or if you have docker and
+ prefer a cleaner environment, install docker-compose with `pip
+ install docker-compose` and run `docker-compose up`.  This will
+ setup a near production ready containerized development environment
+ that runs migrations, with the django development server running on
+ port 8070.
+
+To run one off commands, like shell, you can run
+`docker-compose run web python manage.py shell` or to create root
+user, etc.
+
+## Adding an application
+To add an application to this, add it to the requirements file, add
+its needed settings, include its URLs, and provide any needed template
+overrides.
---
+++ lore/settings.py
@@ -0,0 +1,141 @@
+"""
+Django settings for lore project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.8/ref/settings/
+"""
+import os
+
+import dj_database_url
+import yaml
+
+CONFIG_PATHS = [
+    os.environ.get('LORE_CONFIG', ''),
+    os.path.join(os.getcwd(), 'lore.yml'),
+    os.path.join(os.path.expanduser('~'), 'lore.yml'),
+    '/etc/lore.yml',
+]
+
+
+def load_fallback():
+    """Load optional yaml config"""
+    fallback_config = {}
+    config_file_path = None
+    for config_path in CONFIG_PATHS:
+        if os.path.isfile(config_path):
+            config_file_path = config_path
+            break
+    if config_file_path:
+        with open(config_file_path) as config_file:
+            fallback_config = yaml.safe_load(config_file)
+    return fallback_config
+
+FALLBACK_CONFIG = load_fallback()
+
+
+def get_var(name, default):
+    """Return the settings in a precedence way with default"""
+    return os.environ.get(name, FALLBACK_CONFIG.get(name, default))
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_var(
+    'SECRET_KEY',
+    '36boam8miiz0c22il@3&gputb=wrqr2plah=0#0a_bknw9(2^r'
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = get_var('DEBUG', True)
+
+ALLOWED_HOSTS = get_var('ALLOWED_HOSTS', [])
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'lore.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR + '/templates/'
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'lore.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+# Uses DATABASE_URL to configure with sqlite default:
+# For URL structure:
+# https://github.com/kennethreitz/dj-database-url
+DATABASES = {
+    'default': dj_database_url.parse(
+        get_var('DATABASE_URL', 'sqlite:///{0}'.format(
+            os.path.join(BASE_DIR, 'db.sqlite3')
+        ))
+    )
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+# Serve static files with dj-static
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
