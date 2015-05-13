13b4d4a955d6aad1b9aa224ad5094c063a338376
George Schneeloch <gschneel@mit.edu>
2015-05-13 12:34:06 -0400

    Update name in INSTALLED_APPS

--- lore/settings.py
+++ lore/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taxonomy.apps.TaxonomyConfig',
+    'taxonomy',
 )
 
 MIDDLEWARE_CLASSES = (
