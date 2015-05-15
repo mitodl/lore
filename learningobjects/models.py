659586438016132dd62f4310dc9c593ed04428d9
Shawn Milochik <shawn@milochik.com>
2015-05-15 12:12:45 -0400

    better merge conflict fix

--- learningobjects/models.py
+++ learningobjects/models.py
@@ -1,17 +1,30 @@
+"""
+Learning objects data model
+"""
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Course(models.Model):
-    repository = models.ForeignKey("Repository")
+
+    """
+    A course on edX platform (MITx or residential).
+    """
+    repository = models.ForeignKey('Repository')
     org = models.TextField()
     course_number = models.IntegerField()
     semester = models.TextField()
     import_date = models.DateField()
     imported_by = models.ForeignKey(User)
 
+
 class LearningObject(models.Model):
+    """
+    The units that compose an edX course:
+    chapter, sequential, vertical, problem, video, html, etc.
+    """
     course = models.ForeignKey(Course)
-    learning_object_type = models.ForeignKey("LearningObjectType")
+    learning_object_type = models.ForeignKey('LearningObjectType')
     uuid = models.TextField()
     title = models.TextField()
     description = models.TextField()
@@ -19,17 +32,27 @@ class LearningObject(models.Model):
     path_xml = models.TextField()
     mpath = models.TextField()
     url_path = models.TextField()
-    parent_id = models.ForeignKey("self")
+    parent_id = models.ForeignKey('self')
     copyright = models.TextField()
     xa_nr_views = models.TextField()
     xa_nr_attempts = models.IntegerField()
     xa_avg_grade = models.FloatField()
     xa_histogram_grade = models.FloatField()
 
+
 class LearningObjectType(models.Model):
+    """
+    Learning object type:
+    chapter, sequential, vertical, problem, video, html, etc.
+    """
     name = models.TextField()
-    
+
+
 class Repository(models.Model):
+    """
+    A collection of learning objects
+    that come from (usually tightly-related) courses.
+    """
     name = models.TextField()
     description = models.TextField()
     create_date = models.DateField()
