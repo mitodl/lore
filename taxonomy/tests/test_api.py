8aa974dd70abade9eefe3ab0192247b189996bea
George Schneeloch <gschneel@mit.edu>
2015-05-19 19:32:52 -0400

    Flesh out testing

--- taxonomy/tests/test_api.py
+++ taxonomy/tests/test_api.py
@@ -36,7 +36,9 @@ from taxonomy.api import (
     get_vocabulary,
     remove_term_from_learning_object,
     remove_type_from_vocabulary,
+    TaxonomyAPIException,
 )
+
 
 class TestApi(TestCase):
     """Tests for taxomony API"""
@@ -144,49 +146,154 @@ class TestApi(TestCase):
                           lambda: create_term(456, "label", 9))
         self.assertRaises(IntegrityError,
                           lambda: create_term(self.vocabulary.id, None, 9))
-        self.assertRaises(Vocabulary.DoesNotExist,
-                          lambda: create_term(self.vocabulary.id, "label2",
-                                              4.5))
+        create_term(self.vocabulary.id, "label2", 99.7)
+        # term weight is truncated after being stored in database
+        self.assertEquals(1, Term.objects.filter(weight=99).count())
 
     def test_create_vocabulary(self):
         """
         Test create_vocabulary
         """
+        self.assertEquals([self.vocabulary],
+                          list(Vocabulary.objects.all()))
+
+        v = create_vocabulary(self.repository.id,
+                              "vocabulary",
+                              "description",
+                              False,
+                              "f",
+                              9)
+        self.assertEquals([self.vocabulary, v],
+                          list(Vocabulary.objects.order_by('id')))
+        self.assertRaises(Repository.DoesNotExist,
+                          lambda: create_vocabulary(9,
+                                                    "vocabulary",
+                                                    "description",
+                                                    False,
+                                                    "m",
+                                                    9))
+
+        # int is cast to string
+        create_vocabulary(self.repository.id,
+                          9,
+                          "description",
+                          False,
+                          "f",
+                          9)
+        self.assertEquals("9", Vocabulary.objects.order_by('id').last().name)
+
+        create_vocabulary(self.repository.id,
+                          "name",
+                          9,
+                          False,
+                          "m",
+                          9)
+        self.assertEquals("9", Vocabulary.objects.order_by('id').
+                          last().description)
+
+        # "abc" is cast to boolean True
+        create_vocabulary(self.repository.id,
+                          "vocabulary",
+                          "description",
+                          "abc",
+                          "f",
+                          9)
+        self.assertEquals(True, Vocabulary.objects.order_by('id').last().
+                          required)
+
+        # pass in None, violates constraints
+        self.assertRaises(IntegrityError,
+                          lambda: create_vocabulary(self.repository.id,
+                                                    "vocabulary",
+                                                    "description",
+                                                    None,
+                                                    "m",
+                                                    9))
+
+        self.assertRaises(TaxonomyAPIException,
+                          lambda: create_vocabulary(self.repository.id,
+                                                    "vocabulary",
+                                                    "description",
+                                                    False,
+                                                    ";",
+                                                    9))
 
     def test_delete_term(self):
         """
         Test delete_term
         """
+        self.assertEquals(1, Term.objects.count())
+        delete_term(self.term.id)
+        self.assertEquals(0, Term.objects.count())
+        delete_term(9)
+        delete_term(self.term.id)
+        self.assertEquals(0, Term.objects.count())
 
     def test_delete_vocabulary(self):
         """
         Test delete_vocabulary
         """
+        self.assertEquals(1, Vocabulary.objects.count())
+        delete_vocabulary(9)
+        self.assertEquals(1, Vocabulary.objects.count())
+        delete_vocabulary(self.vocabulary.id)
+        self.assertEquals(0, Vocabulary.objects.count())
 
     def test_get_learning_objects_for_term(self):
         """
         Test get_learning_objects_for_term
         """
+        self.assertEquals([],
+                          list(get_learning_objects_for_term(self.term.id)))
+
+        self.learning_object.terms.add(self.term)
+        self.assertEquals([self.learning_object],
+                          list(get_learning_objects_for_term(self.term.id)))
+
+        self.assertRaises(Term.DoesNotExist,
+                          lambda: get_learning_objects_for_term(9))
 
     def test_get_term(self):
         """
         Test get_term
         """
+        self.assertRaises(Term.DoesNotExist, lambda: get_term(9))
+        self.assertEquals(self.term, get_term(self.term.id))
 
     def test_get_terms_for_learning_object(self):
         """
         Test get_terms_for_learning_object
         """
+        self.assertEquals([],
+                          list(get_terms_for_learning_object(
+                              self.learning_object.id)))
+
+        self.term.learning_objects.add(self.learning_object)
+        self.assertEquals([self.term],
+                          list(get_terms_for_learning_object(
+                              self.learning_object.id)))
+        self.assertRaises(LearningObject.DoesNotExist,
+                          lambda: get_terms_for_learning_object(9))
 
     def test_get_types_for_vocabulary(self):
         """
         Test get_types_for_vocabulary
         """
+        self.assertEquals([self.learning_object_type1.name],
+                          [t.name for t in
+                           get_types_for_vocabulary(self.vocabulary.id)])
+        self.assertRaises(Vocabulary.DoesNotExist,
+                          lambda: get_types_for_vocabulary(3))
 
     def test_get_vocabularies_for_type(self):
         """
         Test get_vocabularies_for_type
         """
+        self.assertEquals([self.vocabulary],
+                          list(get_vocabularies_for_type(
+                              self.learning_object_type1.name)))
+        self.assertRaises(LearningObjectType.DoesNotExist,
+                          lambda: get_vocabularies_for_type("xyz"))
 
     def test_get_vocabulary(self):
         """
@@ -205,8 +312,27 @@ class TestApi(TestCase):
         """
         Test remove_term_from_learning_object
         """
+        self.assertEquals([self.term],
+                          get_terms_for_learning_object(
+                              self.learning_object.id))
+        remove_term_from_learning_object(self.term.id, self.learning_object.id)
+        self.assertEquals([],
+                          get_terms_for_learning_object(
+                              self.learning_object.id))
 
     def test_remove_type_from_vocabulary(self):
         """
         Test remove_type_from_vocabulary
         """
+        self.assertEquals([self.vocabulary.id],
+                          get_vocabularies_for_type(
+                              self.learning_object_type1.name))
+        remove_type_from_vocabulary(self.learning_object_type1.name,
+                                    self.vocabulary.id)
+        self.assertEquals([],
+                          get_vocabularies_for_type(
+                              self.learning_object_type1.name))
+
+        remove_type_from_vocabulary(self.learning_object_type1.name,
+                                    self.vocabulary.id)
+        remove_type_from_vocabulary("xyz", self.vocabulary.id)
