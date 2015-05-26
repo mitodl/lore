130b404ed60e0224c21363dce478d0c0bb4c5269
Shawn Milochik <shawn@milochik.com>
2015-05-26 12:12:39 -0400

    improve test coverage

--- importer/tests/test_views.py
+++ importer/tests/test_views.py
@@ -8,6 +8,9 @@ from django.test import Client
 from django.test.testcases import TestCase
 from django.contrib.auth.models import User
 
+from learningresources.models import LearningResource
+from .test_import import get_course_zip
+
 HTTP_OK = 200
 
 
@@ -32,9 +35,22 @@ class TestViews(TestCase):
         self.assertTrue("Number of learning resources:" in body)
 
     def test_upload_get(self):
-        """Status page."""
+        """GET upload page."""
         client = Client()
         client.login(username=self.username, password=self.password)
         resp = client.get("/importer/upload", follow=True)
         body = resp.content.decode("utf-8")
         self.assertTrue('enctype="multipart/form-data"' in body)
+
+    def test_upload_post(self):
+        """POST upload page."""
+        client = Client()
+        client.login(username=self.username, password=self.password)
+        self.assertTrue(LearningResource.objects.count() == 0)
+        with open(get_course_zip(), "rb") as post_file:
+            client.post(
+                "/importer/upload/",
+                {"course_file": post_file},
+                follow=True
+            )
+        self.assertTrue(LearningResource.objects.count() == 5)
