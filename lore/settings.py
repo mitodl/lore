81b3c918bb17d0964d26f7b6c7be956f709f07b9
Carson Gee <x@carsongee.com>
2015-06-01 16:56:30 -0400

    Add UI to LORE Closes #69

--- lore/settings.py
+++ lore/settings.py
@@ -77,6 +77,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',
+    'bootstrap3',
     'learningresources',
     'importer',
     'ui',
