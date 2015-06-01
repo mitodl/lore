a1be18c495802ab07147b30c2455445817a42fa6
George Schneeloch <gschneel@mit.edu>
2015-06-01 11:59:23 -0400

    Test for __unicode__ behavior

---
+++ learningresources/tests/test_models.py
@@ -0,0 +1,20 @@
+"""
+Tests for learningresources models
+"""
+from __future__ import unicode_literals
+
+from django.test.testcases import TestCase
+
+from learningresources.models import LearningResourceType
+
+
+class TestModels(TestCase):
+    """Tests for learningresources models"""
+
+    def test_unicode(self):
+        """Test for __unicode__ on LearningResourceType"""
+        first = LearningResourceType.objects.create(
+            name="first"
+        )
+
+        self.assertEquals("first", unicode(first))
