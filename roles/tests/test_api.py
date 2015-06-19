d497732de783b23fcef28f4c5d5e849d0be5c9c1
Giovanni Di Milia <gdimilia@mit.edu>
2015-06-19 14:31:27 -0400

    Additional APIs for roles

--- roles/tests/test_api.py
+++ roles/tests/test_api.py
@@ -14,7 +14,8 @@ from learningresources.api import create_repo
 from learningresources.models import Repository
 from learningresources.tests.base import LoreTestCase
 from roles import api
-from roles.permissions import RepoPermission, GroupTypes
+from roles.permissions import RepoPermission, GroupTypes, BaseGroupTypes
+from roles.user_models import UserGroup
 
 
 class TestRoleApi(LoreTestCase):
@@ -25,6 +26,7 @@ class TestRoleApi(LoreTestCase):
     """
     # there are too many self assignment in the init: disabling the check
     # pylint: disable=too-many-instance-attributes, invalid-name
+    # pylint: disable=protected-access
     def __init__(self, *args, **kwargs):
         super(TestRoleApi, self).__init__(*args, **kwargs)
         self.repo_name = 'my little+test repo'
@@ -40,6 +42,16 @@ class TestRoleApi(LoreTestCase):
             self.repo_slug2)
         self.group_curator2 = GroupTypes.REPO_CURATOR.format(self.repo_slug2)
         self.group_author2 = GroupTypes.REPO_AUTHOR.format(self.repo_slug2)
+
+    def sort_user_group(self, user_group_list):
+        """
+        Helper function to sort custom UserGroup objects
+        """
+        # pylint: disable=no-self-use
+        return sorted(
+            user_group_list,
+            key=lambda x: x.username
+        )
 
     def test_roles_init_new_repo_1(self):
         """
@@ -279,4 +291,188 @@ class TestRoleApi(LoreTestCase):
         self.assertListEqual(
             get_perms(self.user, self.repo),
             []
+        )
+
+    def test_list_users_in_repo_no_base_group_type_specified(self):
+        """
+        Test for list_users_in_repo
+        """
+        self.assertListEqual(
+            api.list_users_in_repo(self.repo),
+            [
+                UserGroup(self.user.username, BaseGroupTypes.ADMINISTRATORS)
+            ]
+        )
+        # Remove the user from the administrators.
+        api.remove_user_from_repo_group(
+            self.user,
+            self.repo,
+            GroupTypes.REPO_ADMINISTRATOR
+        )
+        # No users in the repo.
+        self.assertListEqual(
+            api.list_users_in_repo(self.repo),
+            []
+        )
+        # Add user to the curators.
+        api.assign_user_to_repo_group(
+            self.user,
+            self.repo,
+            GroupTypes.REPO_CURATOR
+        )
+        self.assertListEqual(
+            api.list_users_in_repo(self.repo),
+            [
+                UserGroup(self.user.username, BaseGroupTypes.CURATORS)
+            ]
+        )
+        # Add user back to the administrators.
+        api.assign_user_to_repo_group(
+            self.user,
+            self.repo,
+            GroupTypes.REPO_ADMINISTRATOR
+        )
+        self.assertListEqual(
+            self.sort_user_group(
+                api.list_users_in_repo(self.repo)
+            ),
+            self.sort_user_group(
+                [
+                    UserGroup(
+                        self.user.username,
+                        BaseGroupTypes.ADMINISTRATORS
+                    ),
+                    UserGroup(
+                        self.user.username,
+                        BaseGroupTypes.CURATORS
+                    )
+                ]
+            )
+        )
+        # Add another user to the authors.
+        api.assign_user_to_repo_group(
+            self.user_norepo,
+            self.repo,
+            GroupTypes.REPO_AUTHOR
+        )
+        self.assertListEqual(
+            self.sort_user_group(
+                api.list_users_in_repo(self.repo)
+            ),
+            self.sort_user_group(
+                [
+                    UserGroup(
+                        self.user.username,
+                        BaseGroupTypes.ADMINISTRATORS
+                    ),
+                    UserGroup(
+                        self.user.username,
+                        BaseGroupTypes.CURATORS
+                    ),
+                    UserGroup(
+                        self.user_norepo.username,
+                        BaseGroupTypes.AUTHORS
+                    )
+                ]
+            )
+        )
+        # Adding again the same user in the same group
+        # will not create multiple instances.
+        api.assign_user_to_repo_group(
+            self.user_norepo,
+            self.repo,
+            GroupTypes.REPO_AUTHOR
+        )
+        self.assertListEqual(
+            self.sort_user_group(
+                api.list_users_in_repo(self.repo)
+            ),
+            self.sort_user_group(
+                [
+                    UserGroup(
+                        self.user.username,
+                        BaseGroupTypes.ADMINISTRATORS
+                    ),
+                    UserGroup(
+                        self.user.username,
+                        BaseGroupTypes.CURATORS
+                    ),
+                    UserGroup(
+                        self.user_norepo.username,
+                        BaseGroupTypes.AUTHORS
+                    )
+                ]
+            )
+        )
+
+    def test_list_users_in_repo_base_group_type_specified(self):
+        """
+        Test for list_users_in_repo
+        """
+        self.assertListEqual(
+            api.list_users_in_repo(self.repo),
+            [
+                UserGroup(self.user.username, BaseGroupTypes.ADMINISTRATORS)
+            ]
+        )
+        self.assertListEqual(
+            api.list_users_in_repo(self.repo, BaseGroupTypes.ADMINISTRATORS),
+            [
+                UserGroup(self.user.username, BaseGroupTypes.ADMINISTRATORS)
+            ]
+        )
+        self.assertListEqual(
+            api.list_users_in_repo(self.repo, BaseGroupTypes.CURATORS),
+            []
+        )
+        self.assertListEqual(
+            api.list_users_in_repo(self.repo, BaseGroupTypes.AUTHORS),
+            []
+        )
+
+    def test_list_users_in_repo_invalid_group_type_specified(self):
+        """
+        Test for list_users_in_repo
+        """
+        self.assertRaises(
+            api.InvalidGroupType,
+            lambda: api.list_users_in_repo(self.repo, 'foo_type')
+        )
+
+    def test_is_last_admin_in_repo(self):
+        """
+        Test for is_last_admin_in_repo
+        """
+        # By default the repo creator is also administrator
+        self.assertTrue(
+            api.is_last_admin_in_repo(self.user, self.repo)
+        )
+        # Add another user to the administrators.
+        api.assign_user_to_repo_group(
+            self.user_norepo,
+            self.repo,
+            GroupTypes.REPO_ADMINISTRATOR
+        )
+        self.assertFalse(
+            api.is_last_admin_in_repo(self.user, self.repo)
+        )
+        # Remove the first user from the administrators.
+        api.remove_user_from_repo_group(
+            self.user,
+            self.repo,
+            GroupTypes.REPO_ADMINISTRATOR
+        )
+        # Add user to the curators.
+        api.assign_user_to_repo_group(
+            self.user,
+            self.repo,
+            GroupTypes.REPO_CURATOR
+        )
+        # The user is not the last of the admins because he is not admin
+        self.assertFalse(
+            api.is_last_admin_in_repo(self.user, self.repo)
+        )
+        # The other user is indeed the last admin
+        self.assertTrue(
+            api.is_last_admin_in_repo(self.user_norepo, self.repo)
         )
