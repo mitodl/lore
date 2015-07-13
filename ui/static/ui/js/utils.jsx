3806da14b389cc87e62f9a926383a704f2b2bb30
George Schneeloch <gschneel@mit.edu>
2015-07-13 10:45:40 -0400

    Added readonly terms field for quicker taxonomies access

--- ui/static/ui/js/utils.jsx
+++ ui/static/ui/js/utils.jsx
@@ -13,35 +13,14 @@ define("utils", ["jquery", "lodash"], function ($, _) {
         url += "?type_name=" + encodeURI(learningResourceType);
       }
 
-      return $.get(url)
-        .then(function (data) {
-          // some point soon we won't need this extra call
-          var promises = _.map(data.results, function (vocabulary) {
-            return $.get("/api/v1/repositories/" + repoSlug +
-              "/vocabularies/" + vocabulary.slug + "/terms/")
-              .then(function (result) {
-                return {terms: result.results, vocabulary: vocabulary};
-              });
-          });
-
-          return $.when.apply($, promises).then(function () {
-            var args;
-            if (promises.length === 1) {
-              args = [arguments[0]];
-            } else {
-              args = arguments;
-            }
-            return _.map(args, function (obj) {
-              var terms = obj.terms;
-              var vocabulary = obj.vocabulary;
-
-              return {
-                vocabulary: vocabulary,
-                terms: terms
-              };
-            });
-          });
+      return $.get(url).then(function(results) {
+        return _.map(results.results, function(result) {
+          return {
+            vocabulary: result,
+            terms: result.terms
+          };
         });
+      });
     }
   };
 });
