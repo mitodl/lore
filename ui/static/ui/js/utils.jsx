d16a7704807dcef0bf85e0fe1397825ab549e425
George Schneeloch <gschneel@mit.edu>
2015-07-31 17:33:26 -0400

    Implemented Select2 element to refactor select2 widgets

--- ui/static/ui/js/utils.jsx
+++ ui/static/ui/js/utils.jsx
@@ -1,4 +1,5 @@
-define("utils", ["jquery", "lodash", "reactaddons"], function ($, _, React) {
+define("utils", ["jquery", "lodash", "reactaddons", "select2"],
+  function ($, _, React) {
   'use strict';
 
   /**
@@ -59,10 +60,9 @@ define("utils", ["jquery", "lodash", "reactaddons"], function ($, _, React) {
     // onChange is set here to silence React warnings. We're using
     // ifToggled instead.
     render: function() {
-      return <span><input type="checkbox"
-                    value={this.props.value}
-                    checked={this.props.checked}
+      return <span><input {...this.props}
                     onChange={this.handleChange}
+                    type="checkbox"
         /></span>;
     },
     handleChange: function(e) {
@@ -91,6 +91,84 @@ define("utils", ["jquery", "lodash", "reactaddons"], function ($, _, React) {
     }
   });
 
+  /**
+   * Component for Select2 select elements.
+   */
+  var Select2 = React.createClass({
+    render: function() {
+      return <span>
+        <select {...this.props} onChange={function() {}} />
+      </span>;
+    },
+    handleChange: function(e) {
+      if (this.props.onChange !== undefined) {
+        this.props.onChange(e);
+      }
+    },
+    applySelect2: function () {
+      //this function can be used only if the component has been mounted
+      var thiz = this;
+      var isMultiTerms = this.props.multiple;
+      //not allowing the clear for the multi terms dropdown
+      var allowClear = !isMultiTerms;
+      //apply select2 to the right elements
+      var parent = React.findDOMNode(this);
+      var $select = $(parent).find('select').first();
+
+      // HACK to position dropdown below select in DOM so that
+      // it won't attach event handlers all the way up and prevent
+      // correct operation of the scrollbar.
+      var Select2Utils = $.fn.select2.amd.require("select2/utils");
+      var DropdownAdapter = $.fn.select2.amd.require("select2/dropdown");
+      var AttachContainer = $.fn.select2.amd.require(
+        "select2/dropdown/attachContainer");
+      var DropdownSearch = $.fn.select2.amd.require(
+        "select2/dropdown/search");
+      var CustomAdapter = Select2Utils.Decorate(
+        Select2Utils.Decorate(DropdownAdapter, DropdownSearch),
+        AttachContainer
+      );
+
+      $select.select2({
+        multiple: isMultiTerms,
+        data: this.props.options,
+        placeholder: this.props.placeholder,
+        allowClear: allowClear,
+        theme: "bootstrap",
+        dropdownAdapter: CustomAdapter
+      }).val(this.props.values)
+        .trigger('change')
+        .on('change', function (e) {
+          thiz.handleChange(e);
+        });
+    },
+    componentDidMount: function() {
+      this.applySelect2();
+    },
+    componentDidUpdate: function() {
+      this.applySelect2();
+    },
+    componentWillUpdate: function() {
+      var node = React.findDOMNode(this);
+      var $select = $(node).find('select').first();
+      $select.off('change');
+
+      var select2 = $select.data('select2');
+      // HACK: workaround to prevent unselect and related events
+      // from executing.
+      select2.$container.removeClass('select2-container--open');
+      if (select2 !== null && select2 !== undefined) {
+        // HACK: workaround to prevent toggleDropdown from executing on
+        // destroyed object.
+        select2.options.set('disabled', true);
+        select2.destroy();
+      }
+
+      // The select2 widget should get recreated in applySelect2 so
+      // it shouldn't matter that we just made these modifications.
+    }
+  });
+
   return {
     getCollection: _getCollection,
     /**
@@ -115,6 +193,7 @@ define("utils", ["jquery", "lodash", "reactaddons"], function ($, _, React) {
       });
     },
     StatusBox: StatusBox,
-    ICheckbox: ICheckbox
+    ICheckbox: ICheckbox,
+    Select2: Select2
   };
 });
