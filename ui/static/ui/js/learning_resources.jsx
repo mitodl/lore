0f1f1abd185a195b2ab9c06f8e499dedc915837f
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2015-07-31 14:43:16 +0500

    Refreshed search results after editing a learning resource

--- ui/static/ui/js/learning_resources.jsx
+++ ui/static/ui/js/learning_resources.jsx
@@ -156,6 +156,7 @@ define('learning_resources', [
         thiz.setState({
           message: "Form saved successfully!"
         });
+        location.reload();
       }).fail(function () {
         thiz.setState({
           message: {error: "Unable to save form"}
