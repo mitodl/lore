2fb45d6cab3eec52288463646cf7944571a700ad
Carson Gee <x@carsongee.com>
2015-08-14 15:45:33 -0400

    fixup! Added Google Analytics tracking support Closes #509

---
+++ ui/tests/test_ga.py
@@ -0,0 +1,34 @@
+"""
+Tests for repository views
+"""
+from __future__ import unicode_literals
+
+from learningresources.tests.base import LoreTestCase
+
+from django.test.utils import override_settings
+
+
+class TestGoogleAnalytics(LoreTestCase):
+    """Test the Google Analytics setting."""
+
+    @override_settings(GOOGLE_ANALYTICS_ID='')
+    def test_analytics_blank(self):
+        """Verify with the analytics set and not set."""
+        self.login(self.USERNAME)
+        response = self.client.get('/')
+        self.assertNotContains(response, "ga('send', 'pageview');")
+
+    @override_settings(GOOGLE_ANALYTICS_ID=None)
+    def test_analytics_none(self):
+        """Verify with the analytics set and not set."""
+        self.login(self.USERNAME)
+        response = self.client.get('/')
+        self.assertNotContains(response, "ga('send', 'pageview');")
+
+    @override_settings(GOOGLE_ANALYTICS_ID='totally-unique-ga-id')
+    def test_analytics_with_id(self):
+        """Verify with the analytics set and not set."""
+        self.login(self.USERNAME)
+        response = self.client.get('/')
+        self.assertContains(response, "ga('send', 'pageview');")
+        self.assertContains(response, "totally-unique-ga-id")
