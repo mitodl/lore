37e21c0d9619a1b32796b53236b3cd182318f928
Amir Qayyum Khan <amir.qayyum.khan@gmail.com>
2015-09-17 20:44:23 +0500

    update

--- learningresources/migrations/0017_update_learningresource_description_path.py
+++ learningresources/migrations/0017_update_learningresource_description_path.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models, migrations
-import unicodedata
+from learningresources.api import update_description_path
 
 # pylint: skip-file
 
@@ -11,23 +10,19 @@ def update_description_paths(apps, schema_editor):
     """update the description path field"""
     LearningResource = apps.get_model("learningresources", "LearningResource")
     for learning_resource in LearningResource.objects.all():  # pragma: no cover
-        current_description_path = learning_resource.description_path
-        replace_sub_str = u"edX Demonstration Course"
-        replacement_sub_str = u"..."
-
-        new_description_path = (
-            current_description_path.replace(
-                replace_sub_str, replacement_sub_str
-            )
-        )
-        learning_resource.description_path = new_description_path
-        learning_resource.save()
+        if learning_resource.title == "Getting Started":
+            learning_resource.title = "..."
+            learning_resource.save()
+            update_description_path(learning_resource, force_parent_update=True)  # pragma: no cover
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('learningresources', '0021_update_learningresource_description_path'),
+        (
+            'learningresources',
+            '0016_revert_backfill'
+        ),
     ]
 
     operations = [
